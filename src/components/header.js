b2fcc225d502c86fa884b4a7a47d9ba2a7c2a6ba
Ayumi Saito <ayumisaito27@gmail.com>
2017-10-21 23:38:54 +0200

    first commit

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import React ,  { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+export default class Header extends Component {
+	render(){
+		return(
+			<h2> Rijks Museum Collection </h2>
+			);
+	}
+}
